7cedfc30af9547bace34162645c1eed2f0cf43e5
Jishiro <kronosiraxx@gmail.com>
2025-07-03 16:31:29 -0500

    credenciales 2.0

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,13 +1,16 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$clave = "miclave123";
-$bd = "Sistema_De_Control";
-$puerto = 3307;
+$host = "localhost"; // Hostinger usa localhost
+$usuario = "u767975160_SistemaControl";
+$clave = "m4x40NmL$";
+$bd = "u767975160_SistemaControl";
 
-$conn = new mysqli($host, $usuario, $clave, $bd, $puerto);
+// Nota: no necesitas puerto en Hostinger, así que lo quitamos
+$conn = new mysqli($host, $usuario, $clave, $bd);
 
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("❌ Error de conexión: " . $conn->connect_error);
 }
+
+// Asegura que el conjunto de caracteres sea UTF-8
+$conn->set_charset("utf8");
 ?>
